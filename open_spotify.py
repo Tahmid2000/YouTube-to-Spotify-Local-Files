ff8b206d0b8610507d76a7534862fbac92278f03
Tahmid Imran <tahmidimran1@gmail.com>
2024-02-04 16:35:10 -0600

    Make coords variables

--- open_spotify.py
+++ open_spotify.py
@@ -1,15 +1,6 @@
 import pyautogui
 import time
 
-def position_finder():
-    try:
-        while True:
-            x, y = pyautogui.position()
-            print(f"\rThe current mouse position is {x}, {y}", end="")
-            time.sleep(0.2)  # Adjust the sleep time as needed
-    except KeyboardInterrupt:
-        print("\nDone.")
-
 def open_spotify_app():
     pyautogui.press('volumeup')
     pyautogui.hotkey('command', 'space')
@@ -17,17 +8,20 @@ def open_spotify_app():
     pyautogui.press('enter')
 
 def open_local_files_playlist():
-    pyautogui.moveTo(140, 320)
+    local_files_point = [140, 320]
+    pyautogui.moveTo(local_files_point[0], local_files_point[1])
     pyautogui.click()
     pyautogui.click()
 
 def scroll_to_bottom():
-    pyautogui.moveTo(617,633)
+    middle_of_screen = [617, 633]
+    pyautogui.moveTo(middle_of_screen[0], middle_of_screen[1])
     time.sleep(2)
     pyautogui.scroll(-500)
     
 def like_song():
-    pyautogui.moveTo(1549,889)
+    newest_file_like_button = [1549, 889]
+    pyautogui.moveTo(newest_file_like_button[0],newest_file_like_button[1])
     time.sleep(2)
     pyautogui.click()
 
